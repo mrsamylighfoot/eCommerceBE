40ca10bbd88af341ec3a76a8bda283a35e45c4a2
Amy Lightfoot <101309406+mrsamylighfoot@users.noreply.github.com>
2022-04-29 23:14:27 -0400

    update model index.js

--- models/index.js
+++ models/index.js
@@ -4,6 +4,13 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+Products.belongsToMany(Category, {
+  through: {
+    model: Name,
+    unique: false
+  },
+  as: 'product_category'
+});
 // Products belongsTo Category
 
 // Categories have many Products
