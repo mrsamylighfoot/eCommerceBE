564306337d3c07318ff7c8db37f68cd5ebb97605
Amy Lightfoot <101309406+mrsamylighfoot@users.noreply.github.com>
2022-05-02 23:01:39 -0400

    updated server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const express = require('express');
-const routes = require('./routes');
+const express = require("express");
 // import sequelize connection
+const sequelize = require("./config/connection");
+
+const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,6 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
 });
